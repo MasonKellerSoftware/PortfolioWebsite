a1cec13f3acfbfb7585f0bcf1927b3153454b912
Mason Keller <mmk0062@auburn.edu>
2025-06-17 00:28:37 -0500

    TechStack Update

--- src/views.py
+++ src/views.py
@@ -4,20 +4,45 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    Tech_images =[
-        'Bootstrap.svg',
-        'Flask.svg',
-        'HTML5.svg',
-        'Java.svg',
-        'React.svg',
-        'Python.svg',
-        'SQLAlchemy.svg',
-        'C# (CSharp).svg',
-        'C++ (CPlusPlus).svg',
-        'Selenium.svg',
-        'Tailwind CSS.svg',
+    techstack = [
+        {
+            "section": "Programming Languages",
+            "items": [
+                {"name": "Python", "img": "Python.svg"},
+                {"name": "C#", "img": "C# (CSharp).svg"},
+                {"name": "C++", "img": "C++ (CPlusPlus).svg"},
+                {"name": "Java", "img": "Java.svg"},
+                {"name": "HTML5", "img": "HTML5.svg"},
+                {"name": "CSS3", "img": "CSS3.svg"},  # Fixed SVG file
+                {"name": "Sass", "img": "Sass.svg"},
+            ]
+        },
+        {
+            "section": "Frameworks and Libraries",
+            "items": [
+                {"name": "React", "img": "React.svg"},
+                {"name": "Bootstrap", "img": "Bootstrap.svg"},
+                {"name": "Tailwind CSS", "img": "Tailwind CSS.svg"},
+                {"name": "Flask", "img": "Flask.svg"},
+                {"name": "Django", "img": "Django.svg"},
+                {"name": "SQLAlchemy", "img": "SQLAlchemy.svg"},
+                {"name": "Selenium", "img": "Selenium.svg"}  # Moved here
+            ]
+        },
+        {
+            "section": "Collaboration and Version Control",  # Fixed typo
+            "items": [
+                {"name": "Git", "img": "Git.svg"},
+                {"name": "Slack", "img": "Slack.svg"},        # Fixed SVG
+                {"name": "Jira", "img": "Jira.svg"},          # Fixed SVG
+                {"name": "DevOps", "img": "Azure.svg"}  # Fixed name/SVG
+            ]
+        }
     ]
-    return render_template("home.html", tech_images=Tech_images)
+
+    return render_template("home.html", techstack=techstack)
+
+
 
 @views.route('/about')
 def about():
